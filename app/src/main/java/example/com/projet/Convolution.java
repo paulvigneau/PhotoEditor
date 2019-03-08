4c8fb874c60bacf246cddb398fadb928bc98ca7f
ctarmil <chaima.tarmil@etu.u-bordeaux.fr>
2019-03-08 16:02:33 +0100

    version that compiles

--- app/src/main/java/example/com/projet/Convolution.java
+++ app/src/main/java/example/com/projet/Convolution.java
@@ -20,7 +20,7 @@ public class Convolution extends Filter {
     public Convolution(MainActivity main, Image image) {
         super(main, image);
         this.matrix = Matrix.PREWITT;
-        this.length = 15;
+        this.length = 3;
     }
 
     @Override
@@ -28,54 +28,71 @@ public class Convolution extends Filter {
         JavaApply();
     }
 
-    private void JavaApply(){
+    private void JavaApply() {
         this.oldpixels = super.imageSrc.getPixels();
         this.width = super.imageSrc.getWidth();
         this.height = super.imageSrc.getHeight();
         this.M = this.matrix.getType().generate(this.length);
 
         int[] pixels = super.imageOut.getPixels();
+        if (matrix == Matrix.PREWITT || matrix == Matrix.SOBEL) {
+            for (int y = 0; y < this.height; y++) {
+                for (int x = 0; x < this.width; x++) {
+                    int value[] = getValue(x, y, true);
+                    value[0] = (int) Math.sqrt(value[0] * value[0] + value[3] * value[3]);
+                    value[1] = (int) Math.sqrt(value[1] * value[1] + value[4] * value[4]);
+                    value[2] = (int) Math.sqrt(value[2] * value[2] + value[5] * value[5]);
+                    pixels[x + y * this.width] = Color.argb(255, (value[0]), (value[1]), (value[2]));
+                }
+            }
 
-        for(int y=0; y<this.height; y++){
-            for(int x=0; x<this.width; x++){
-                float value[] = getValue(x, y);
-                pixels[x + y * this.width] = Color.argb(255, (int)(value[0]), (int)(value[1]), (int)(value[2]));
+        } else {
+            for (int y = 0; y < this.height; y++) {
+                for (int x = 0; x < this.width; x++) {
+                    int value[] = getValue(x, y,false);
+                    pixels[x + y * this.width] = Color.argb(255, (value[0]), (value[1]), (value[2]));
+                }
             }
         }
-
         super.imageOut.setPixels(pixels);
     }
 
-    public float[] getValue(int indexX, int indexY){
+    public int[] getValue(int indexX, int indexY, boolean ps) {
+        float value[];
+        if(ps) value = new float[6];
+        else value = new float[3];
+        indexX -= this.length / 2;
+        indexY -= this.length / 2;
 
-
-        float value[] = new float[3];
-
-        indexX -= this.length /2;
-        indexY -= this.length /2;
-
-        int size = 0;
-        for(int x = 0; x < this.length; x++){
-            for(int y = 0; y < this.length; y++) {
+        for (int x = 0; x < this.length; x++) {
+            for (int y = 0; y < this.length; y++) {
                 int localX = indexX + x;
                 int localY = indexY + y;
-                int X = Math.abs(localX - (localX/(width-1)) * (localX%(width-1))*2);
-                int Y = Math.abs(localY - (localY/(height-1)) * (localY%(height-1))*2);
+                int X = Math.abs(localX - (localX / (width - 1)) * (localX % (width - 1)) * 2);
+                int Y = Math.abs(localY - (localY / (height - 1)) * (localY % (height - 1)) * 2);
                 int index = X + Y * width;
-
+                int size = x + y * this.length;
                 float mult = M[size];
                 int color = oldpixels[index];
                 value[0] += Color.red(color) * mult;
                 value[1] += Color.green(color) * mult;
                 value[2] += Color.blue(color) * mult;
-
-                size++;
+                if(ps){
+                    size = y+x*this.length;
+                    mult = M[size];
+                    value[3] += Color.red(color) * mult;
+                    value[4] += Color.green(color) * mult;
+                    value[5] += Color.blue(color) * mult;
+                }
             }
         }
-        return value;
+        int[] val;
+        if(ps) val = new int[]{(int) value[0], (int) value[1], (int) value[2],(int)value[3],(int)value[4],(int)value[5]};
+        else val = new int[]{(int) value[0], (int) value[1], (int) value[2]};
+        return val;
     }
 
-    private void RenderScriptApply(){
+    private void RenderScriptApply() {
         RenderScript rs = RenderScript.create(super.main);
 
         Allocation input = Allocation.createFromBitmap(rs, super.imageSrc.getBitmap());
@@ -90,7 +107,7 @@ public class Convolution extends Filter {
         blurScript.set_matrixSize(this.length);
 
         float[] matrix = blurScript.get_matrix();
-        for(int index = 0; index < matrix.length; index++){
+        for (int index = 0; index < matrix.length; index++) {
             System.out.println(matrix[index]);
         }
         System.out.println("size :" + blurScript.get_matrixSize());
@@ -116,11 +133,11 @@ public class Convolution extends Filter {
         this.matrix = matrix;
     }
 
-    public int getLength(){
+    public int getLength() {
         return this.length;
     }
 
-    public void setLength(int length){
+    public void setLength(int length) {
         this.length = length;
     }
 }
